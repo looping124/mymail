93c5e1ac3822219ef76d0ca8d6a44e782578378e
Cedric Le Net <lenetcedric@gmail.com>
2021-11-24 11:41:57 +0100

    midi

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -1,15 +1,34 @@
 class EmailsController < ApplicationController
   def index
     @emails = Email.all
+    # .order('updated_at DESC')
   end
 
   def new
+    
   end
 
   def create
+     @email = Email.create(object:Faker::Movies::StarWars.quote,body:Faker::Lorem.paragraph(sentence_count: 5))
+    
+
+    respond_to do |format|
+      format.html do 
+        #code en cas de requête classique
+        redirect_to root_path
+      end
+      format.js do
+        #code en cas de requête AJAX
+      end
+    end
   end
 
   def show
+    @email = Email.find(params[:id])
+    respond_to do |format|
+      format.html { redirect_to email_path(params[:id]) }
+      format.js { }
+    end
   end
 
   def edit
