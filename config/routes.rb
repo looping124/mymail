86bb0425b023142a4f9ae1d6e7116d43557f0736
Cedric Le Net <lenetcedric@gmail.com>
2021-11-24 09:23:58 +0100

    controler and model

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'emails/index'
+  get 'emails/new'
+  get 'emails/create'
+  get 'emails/show'
+  get 'emails/edit'
+  get 'emails/update'
+  get 'emails/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
